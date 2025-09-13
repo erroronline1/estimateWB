42858347a16bdf3c020fff755a3819c38b178ca6
erroronline1 <dev@erroronline.one>
2025-09-13 19:01:18 +0200

    reduced cluttering version files

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 from setuptools import setup
-import os
-from freecad.estimateWB.version import __version__
 # name: this is the name of the distribution.
 # Packages using the same name here cannot be installed together
 
-version_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                            "freecad", "estimateWB", "version.py")
-with open(version_path) as fp:
-    exec(fp.read())
-
-setup(name='freecad.estimateWB',
-      version=str(__version__),
-      packages=['freecad',
-                'freecad.estimateWB'],
-      maintainer="error on line 1",
-      maintainer_email="dev@erroronline.one",
-      url="https://github.com/erroronline1/estimateWB",
-      description="estimate material quantity",
-      install_requires=[],  # should be satisfied by FreeCAD's system dependencies already
-      include_package_data=True)
+setup(
+    name = 'freecad.estimateWB',
+    version = "0.1.4",
+    packages = ['freecad',
+        'freecad.estimateWB'],
+    maintainer = "error on line 1",
+    maintainer_email = "dev@erroronline.one",
+    url = "https://github.com/erroronline1/estimateWB",
+    description = "estimate the volume & weight of parts",
+    install_requires = [],  # should be satisfied by FreeCAD's system dependencies already
+    include_package_data = True)
--- versionupdate.py
+++ versionupdate.py
@@ -3,64 +3,59 @@ import re
 import os
 from datetime import datetime
 
-def helparg(workbench):
+def helparg():
     print(f'''
 usage: versionupdate.py [ -h  | --help ]
                         [ -v  | --version ] VERSION
                         [ -d  | --description ] DESCRIPTION
 
 updates the workbench version number and description on all relevant issued places like
-* freecad/{workbench}/version.py
 * manifest.ini
 * package.xml
 * setup.py
 
 because it affects so many different places. not in the readme though
-
-called from the root folder of the workbench it detects the name within
-the freecad-directory on its own. only quoted values are replaced within py files.
 ''')
     
-def update(workbench, version, description):
+def update(version, description):
     version = version.strip() if version else version
     description = description.strip() if description else description
 
     root_path = os.path.dirname(os.path.realpath(__file__))
-    wb_path = os.path.realpath(current_path + '/freecad/' + workbench)
-
     root_files = os.listdir(root_path)
-    wb_files = os.listdir(wb_path)
+
+    '''
+    # detect workbench in calling directory to process occasional containes files to alter, if any
+    try:
+        wb_path = os.listdir(os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + '/freecad'))[0]
+    except:
+        wb_path = None
+    '''
 
     if 'manifest.ini' in root_files:
-        path = os.path.realpath(current_path + '/manifest.ini')
+        path = os.path.realpath(root_path + '/manifest.ini')
         if version:
             rewrite(path, r'version=.+', 'version=' + version)
         if description:
             rewrite(path, r'description=.+', 'description=' + description)
-        print(path + ' bumped')
+        print(path + ' updated')
     if 'package.xml' in root_files:
-        path = os.path.realpath(current_path + '/package.xml')
+        path = os.path.realpath(root_path + '/package.xml')
         if version:
             rewrite(path, r'<version>.+?<\/version>', '<version>' + version + '</version>')
         if description:
              rewrite(path, r'<description>.+?<\/description>', '<description>' + description + '</description>')  
         rewrite(path, r'<date>.+?<\/date>', '<date>' + datetime.today().strftime('%Y-%m-%d') + '</date>')
-        print(path + ' bumped')
+        print(path + ' updated')
     if 'setup.py' in root_files:
-        path = os.path.realpath(current_path + '/setup.py')
+        path = os.path.realpath(root_path + '/setup.py')
         if version:
             # does not replace imported variables by searching for literal values within quotes only
-            rewrite(path, r'version\s{0,}=\s{0,}["\'].+?["\']', 'version="' + version + '"')
+            rewrite(path, r'version\s{0,}=\s{0,}["\'].+?["\']', 'version = "' + version + '"')
         if description:
-             rewrite(path, r'description\s{0,}=\s{0,}["\'].+?["\']', 'description="' + description + '"')
-        rewrite(path, r'<date>.+?<\/date>', '<date>' + datetime.today().strftime('%Y-%m-%d') + '</date>')
-        print(path + ' bumped')
+             rewrite(path, r'description\s{0,}=\s{0,}["\'].+?["\']', 'description = "' + description + '"')
+        print(path + ' updated')
 
-    if 'version.py' in wb_files:
-        path = os.path.realpath(current_path + '/freecad/' + workbench + '/version.py')
-        if version:
-            rewrite(path, r'__version__ = ".+?"', '__version__ = "' + version + '"')
-        print(path + ' bumped')
     print('please check if everything fits')
 
 def rewrite(path, re_search, re_replace):
@@ -94,14 +89,8 @@ if __name__ == '__main__':
         if opt == 'd' and bool(arg):
             description = arg[0][1]
             params = params.replace(''.join(arg[0]), '')
-
-    workbench = None
-    # detect workbench in calling directory
-    current_path = os.path.dirname(os.path.realpath(__file__))
-    if 'freecad' in os.listdir(current_path):
-        workbench = os.listdir(os.path.realpath(current_path + '/freecad'))[0]
     
-    if workbench and (version or description):
-        update(workbench, version, description)
+    if version or description:
+        update(version, description)
     else:
-        helparg(workbench)
+        helparg()
