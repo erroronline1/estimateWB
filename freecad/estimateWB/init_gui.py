add7999dbe6eb8b4a165e285750ed47584950e85
erroronline1 <dev@erroronline.one>
2025-09-14 02:27:45 +0200

    settings now as one menu-item, toolbar, way better formulated construction

--- freecad/estimateWB/init_gui.py
+++ freecad/estimateWB/init_gui.py
@@ -1,52 +1,56 @@
-import os
-from . import ICONPATH, tools
+from .resources import icon, materials, LANG
 
 import FreeCADGui
 
 class estimateWB(FreeCADGui.Workbench):
-	
+	"""
+		this is the main workbench class that embeds the menu and toolbar buttons with assigned functions
+	"""
+
 	MenuText = "Estimate"
-	ToolTip = tools.LANG.chunk("wbToolTip")[0] #"Display a body's volume or weight to estimate costs of printing"
-	Icon = os.path.join(ICONPATH, "icon.svg")
+	ToolTip = LANG.chunk("wbToolTip")
+	Icon = icon("icon")
 	
-	commands = [
-		"Report_console", "Report_popup",
-		"Scale_qmm", "Scale_qcm", "Scale_qm",
-		"Weight_g", "Weight_kg", "Weight_lb",
-		"Estimate_Volume", "Estimate_Weight_Custom",
-		]
-	
-	for type in tools.materials:
-		commands.append(f"Estimate_{type}_Weight")
-
 	def Initialize(self):
-		"""This function is executed when FreeCAD starts"""
-		from . import commands #, import here all the needed files that create your FreeCAD commands
+		"""
+			This function is executed when FreeCAD starts
+		"""
 
-		self.appendToolbar("Report", self.commands[:2])
-		self.appendMenu("Report", self.commands[:2])
-		FreeCADGui.addCommand("Report_console", commands.Toggle_Report("console"))
-		FreeCADGui.addCommand("Report_popup", commands.Toggle_Report("popup"))
+		# TMYK: import all the needed files that contain your FreeCAD commands here
+		from . import commands 
 
-		self.appendToolbar("Scale", self.commands[2:5])
-		self.appendMenu("Scale", self.commands[2:5])
-		for type in ['mm', 'cm', 'm']:
-			FreeCADGui.addCommand(f"Scale_q{type}", commands.Set_Scale(type))
+		# setting commands
+		fn = {
+			"Report_console": commands.Set_estimateSettings("reportOutput", "console"),
+			"Report_popup": commands.Set_estimateSettings("reportOutput", "popup"),
+			"Scale_qmm": commands.Set_estimateSettings("sizeScale", "mm"),
+			"Scale_qcm": commands.Set_estimateSettings("sizeScale", "cm"),
+			"Scale_qm": commands.Set_estimateSettings("sizeScale", "m"),
+			"Weight_g": commands.Set_estimateSettings("weightUnit", "g"),
+			"Weight_kg": commands.Set_estimateSettings("weightUnit", "kg"),
+			"Weight_lb": commands.Set_estimateSettings("weightUnit", "lb")
+		}
+		# TMYK: the first parameter is visible e.g as the menu entry, second parameter is a list of the assigned commands names
+		# you could as well assign a manual list as second parameter
+		self.appendToolbar(LANG.chunk("settingsMenuText"), list(fn.keys()))
+		self.appendMenu(LANG.chunk("settingsMenuText"), list(fn.keys()))
+		# you could as well add every command linewise, with the first parameter being the assigned command name, the second the respective function call
+		for type in fn:
+			FreeCADGui.addCommand(type, fn[type])
 
-		# Handles the weight units (move to selecting based on preferences in freeCAD?)
-		# I can foresee some cases in which you'd want to work in one unit system, yet
-		# display the volume or weight in a different unit system.
-		self.appendToolbar("Weight Units", self.commands[5:8])
-		self.appendMenu("Weight Units", self.commands[5:8])
-		for massUnit in ['lb', 'kg', 'g']:
-			FreeCADGui.addCommand(f"Weight_{massUnit}", commands.Set_Weight_Unit(massUnit))
+		# estimate commands
+		# TMYK: but assigning like this is actually cool, because it is dynamically extendable
+		fn = {
+			"Estimate_Volume": commands.Estimate_Volume(),
+			"Estimate_Weight_Custom": commands.Estimate_Weight_Custom()
+		}
+		for type in materials:
+			fn[f"Estimate_{type}_Weight"] = commands.Estimate_Weight(type)
 
-		self.appendToolbar("Estimate", self.commands[8:])
-		self.appendMenu("Estimate", self.commands[8:])
-		FreeCADGui.addCommand("Estimate_Volume", commands.Estimate_Volume())
-		FreeCADGui.addCommand("Estimate_Weight_Custom", commands.Estimate_Weight_Custom())
-		for type in tools.materials:
-			FreeCADGui.addCommand(f"Estimate_{type}_Weight", commands.Estimate_Weight(type))
+		self.appendToolbar(LANG.chunk("estimateMenuText"), list(fn.keys()))
+		self.appendMenu(LANG.chunk("estimateMenuText"), list(fn.keys()))
+		for type in fn:
+			FreeCADGui.addCommand(type, fn[type])
 
 	def Activated(self):
 		pass
@@ -60,7 +64,6 @@ class estimateWB(FreeCADGui.Workbench):
 
 	# This function is mandatory if this is a full Python workbench
 	# This is not a template, the returned string should be exactly "Gui::PythonWorkbench"
-	
 	def GetClassName(self): 
 		return "Gui::PythonWorkbench"
 
